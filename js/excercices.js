62d8bf5094f21a42345839aad962528c2bb5a7b1
Edgar Andres Silvera <EdgarAndres.Silvera@promutuel.ca>
2018-05-04 20:48:28 +0100

    add listes outils et lignes + methode pour delete

--- js/excercices.js
+++ js/excercices.js
@@ -1,5 +1,12 @@
+var objectSelected = null;
+
 $(function() {
     initListeTerrains();
+    initListeJoueurs();
+    initListeOutils();
+    initListeLignes();
+
+    initButtons();
 });
 
 
@@ -21,6 +28,71 @@ function changerTerrain(color){
     $('.terrain-space').css('background-color', color);
 }
 
+function initListeJoueurs() {
+    var contenu = '';
+
+    listeObjects.liste_joueurs.forEach(function (item) {
+        contenu += '<li class="list-group-item joueur-li"><div class="joueurs-li-div" onclick="ajouterJoueur(\'' + item.name + '\', \'' + item.image  + '\');">' + item.name + '</div></li>';
+    });
+
+    $('#ul-liste-joueurs').html(contenu);
+}
+
+function ajouterJoueur(joueurName, joueurImage) {
+    var noJoueurs = $('.terrain-space')[0].children.length + 1;
+    var contenu = '<div id="drag-joueur-' + noJoueurs +  '" class="draggable drag" onclick="selectObject(\'drag-joueur-' + noJoueurs + '\')">' +
+                  '<p>' + noJoueurs + '</p>' +
+                  '</div>';
+    $('.terrain-space').append(contenu);
+}
+
+function initListeOutils() {
+    var contenu = '';
+
+    listeObjects.liste_outils.forEach(function (item) {
+        contenu += '<li class="list-group-item outil-li"><div class="outil-li-div" onclick="ajouterOutil(\'' + item.name + '\', \'' + item.image  + '\');">' + item.name + '</div></li>';
+    });
+
+    $('#ul-liste-outils').html(contenu);
+}
+
+function ajouterOutil(outilName, outilImage) {
+    var noJoueurs = $('.terrain-space')[0].children.length + 1;
+    var contenu = '<div id="drag-outil-' + noJoueurs +  '" class="draggable drag">' +
+        '<p>' + noJoueurs + '</p>' +
+        '</div>';
+    $('.terrain-space').append(contenu);
+}
+
+function initListeLignes() {
+    var contenu = '';
+
+    listeObjects.liste_lignes.forEach(function (item) {
+        contenu += '<li class="list-group-item ligne-li"><div class="ligne-li-div" onclick="ajouterLigne(\'' + item.name + '\', \'' + item.image  + '\');">' + item.name + '</div></li>';
+    });
+
+    $('#ul-liste-lignes').html(contenu);
+}
+
+function ajouterLigne(ligneName, ligneImage) {
+    var noJoueurs = $('.terrain-space')[0].children.length + 1;
+    var contenu = '<div id="drag-lgn-' + noJoueurs +  '" class="draggable drag">' +
+        '<p>' + noJoueurs + '</p>' +
+        '</div>';
+    $('.terrain-space').append(contenu);
+}
+
+function deleteObject() {
+    objectSelected.remove()
+}
+
+function selectObject(dragId){
+    objectSelected = $('#' + dragId);
+}
+
+function initButtons() {
+    //$('#delete').prop('disabled', true);
+}
 
 // target elements with the "draggable" class
 interact('.draggable')
@@ -40,13 +112,13 @@ interact('.draggable')
     onmove: dragMoveListener,
     // call this function on every dragend event
     onend: function (event) {
-      var textEl = event.target.querySelector('p');
-
-      /*textEl && (textEl.textContent =
-        'moved a distance of '
-        + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-                     Math.pow(event.pageY - event.y0, 2) | 0))
-            .toFixed(2) + 'px');*/
+      // var textEl = event.target.querySelector('p');
+      //
+      // textEl && (textEl.textContent =
+      //   'moved a distance of '
+      //   + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+      //                Math.pow(event.pageY - event.y0, 2) | 0))
+      //       .toFixed(2) + 'px');
     }
   });
 
